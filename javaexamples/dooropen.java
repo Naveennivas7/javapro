5c4812dd3f676c851360a69320996b2fb8fd3fd0
Naveennivas7 <naveennivaskrishna@gmail.com>
2025-07-09 08:45:20 +0530

    iunfewiic

---
+++ javaexamples/dooropen.java
@@ -0,0 +1,14 @@
+package javaexamples;
+
+public class dooropen {
+    public static void main(String [] args){
+        int n=9;
+        int k=((int)Math.sqrt(n));
+        for(int i=1;i<=k;i++){
+            
+            System.out.println(i*i);
+
+        } 
+    }
+    
+}
